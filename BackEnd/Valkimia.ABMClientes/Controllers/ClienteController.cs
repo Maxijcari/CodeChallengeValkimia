ade2d5a8865168e6feac3600b418d98f852cb405
mcari <maxijcari.estudio@gmail.com>
2023-06-12 21:07:27 -0300

    Creación de Controladores Cliente y Factura

---
+++ BackEnd/Valkimia.ABMClientes/Controllers/ClienteController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Valkimia.ABMClientes.Data;
+using Valkimia.ABMClientes.Models;
+
+namespace Valkimia.ABMClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClienteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateClient([FromBody] Cliente cliente)
+        {
+            try
+            {
+                Cliente MyCliente = new Cliente
+                {
+                    Id = Guid.NewGuid(),
+                    Nombre = cliente.Nombre,
+                    Apellido = cliente.Apellido,    
+                    Domicilio = cliente.Domicilio,
+                    Email = cliente.Email,
+                    Password = cliente.Password,
+                    Habilitado = cliente.Habilitado,
+                    CiudadId = cliente.CiudadId,
+                };
+    
+                _context.Add(MyCliente);
+                _context.SaveChanges();
+                return Ok(MyCliente);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(ex.ToString());
+
+            }
+            
+        }
+        [HttpGet]
+        public IActionResult GetAllClients()
+        {
+            try 
+            {
+                List<Cliente> Clientes = _context.Clientes.ToList();
+                string jsonClientes = JsonConvert.SerializeObject(Clientes);
+                if (jsonClientes != null)
+                {
+                    return Json(jsonClientes);
+                }
+                else
+                {
+                    return NotFound("No se encontró ningun cliente");
+                }
+            }catch(Exception ex) 
+            {
+                return Json(ex.ToString());
+            }
+        }
+
+        [HttpPut]
+        public IActionResult ModifyClient()
+        {
+            return View();
+        }
+        [HttpDelete]
+        public IActionResult DeleteClient() 
+        {
+            return View(); 
+        }
+    }
+}
---
+++ BackEnd/Valkimia.ABMClientes/Controllers/FacturaController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Valkimia.ABMClientes.Data;
+using Valkimia.ABMClientes.Models;
+
+namespace Valkimia.ABMClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public FacturaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateFactura([FromBody] Factura factura)
+        {
+            try
+            {
+                Factura MyFactura = new Factura
+                {
+                    Id = Guid.NewGuid(),
+                    ClienteId = factura.ClienteId,
+                    Fecha = factura.Fecha,
+                    Detalle = factura.Detalle,
+                    Importe = factura.Importe,
+                };
+                _context.Add(MyFactura);
+                _context.SaveChanges();
+                return Ok(MyFactura);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.ToString());
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllFacturas()
+        {
+            try
+            {
+                List<Factura> Facturas = _context.Facturas.ToList();
+                string jsonFacturas = JsonConvert.SerializeObject(Facturas);
+                if (jsonFacturas != null)
+                {
+                    return Json(jsonFacturas);
+                }
+                else
+                {
+                    return NotFound("No se encontró ninguna factura");
+                }
+
+            }
+            catch (Exception ex) 
+            {
+                return Json(ex.ToString());
+            }
+        }
+        [HttpPut]
+        public IActionResult ModifyFactura()
+        {
+            return View();
+        }
+        [HttpDelete]
+        public IActionResult DeleteFactura()
+        {
+            return View();
+        }
+    }
+}
