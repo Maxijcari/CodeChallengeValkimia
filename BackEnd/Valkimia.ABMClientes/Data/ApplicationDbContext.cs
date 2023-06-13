a09c4ff30c799d5989840c9c5a10f447d3d9db27
mcari <maxijcari.estudio@gmail.com>
2023-06-12 21:04:26 -0300

    Creación del ApplicationDbContext

--- BackEnd/Valkimia.ABMClientes/Data/ApplicationDbContext.cs
+++ BackEnd/Valkimia.ABMClientes/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Valkimia.ABMClientes.Models;
 
 namespace Valkimia.ABMClientes.Data
 {
@@ -8,5 +9,27 @@ namespace Valkimia.ABMClientes.Data
         {
                 
         }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<Ciudad> Ciudades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>()
+                .HasOne(c => c.Ciudad)
+                .WithMany(ci => ci.Clientes)
+                .HasForeignKey(c => c.CiudadId);
+
+            modelBuilder.Entity<Factura>()
+                .Property(i => i.Importe)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Factura>()
+                .HasOne(f => f.Cliente)
+                .WithMany(c => c.Facturas)
+                .HasForeignKey(f => f.ClienteId);
+        }
+        
+
     }
 }
