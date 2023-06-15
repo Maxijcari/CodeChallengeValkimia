83765487e5d5b5326dc4b251aed8ba71e4260344
mcari <maxijcari.estudio@gmail.com>
2023-06-15 01:12:11 -0300

    Se agrega el Helper para proteccion de Password y los Requests para mapear los valores q vienen del front y se agrega la libreria BCrypt

---
+++ BackEnd/Valkimia.ABMClientes/Helpers/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BCrypt.Net;
+
+
+namespace Valkimia.ABMClientes.Helpers
+{
+    public class Helper
+    {
+        public string HashPassword(string password)
+        {
+            // Generar un salt aleatorio
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+
+            // Hashear el password con el salt generado
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+
+            return hashedPassword;
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            // Verificar si el password coincide con el hash almacenado
+            bool passwordMatches = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+
+            return passwordMatches;
+        }
+    }
+}
---
+++ BackEnd/Valkimia.ABMClientes/Requests/ClienteRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valkimia.ABMClientes.Models;
+
+namespace Valkimia.ABMClientes.Requests
+{
+    public class ClienteRequest
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Domicilio { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool Habilitado { get; set; }
+        public Guid CiudadId { get; set; }
+    }
+}
